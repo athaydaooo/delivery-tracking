0d53b0f531410f03d7e30a2ec5496f236aaa4fdf
Lucas Athayde <athaydaooo@gmail.com>
2025-06-26 10:41:32 -0300

    refactor: logger removed from service

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,9 +1,7 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { QueueService } from '@shared/queue/queue.service';
-import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Repository } from 'typeorm';
-import { Logger } from 'winston';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { Orders } from './entities/order.entity';
 
@@ -13,8 +11,6 @@ export class OrdersService {
     @InjectRepository(Orders)
     private readonly ordersRepository: Repository<Orders>,
     private readonly queueService: QueueService,
-    @Inject(WINSTON_MODULE_PROVIDER)
-    private readonly logger: Logger,
   ) {}
 
   create(createOrderDto: CreateOrderDto) {
